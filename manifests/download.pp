ee19cd3b48022d5c1c12240a0eea29a64590da5e
Greg Lund-Chaix <greg@tag1consulting.com>
2016-09-15 13:24:14 -0700

    Only use www user for xhgui

--- manifests/download.pp
+++ manifests/download.pp
@@ -2,7 +2,7 @@
 class xhgui::download(
   $dir,
   $version,
-  $user       = $xhgui::params::www_user,
+  $user       = undef,
   $repository = 'https://github.com/perftools/xhgui.git'
 ) {
   # Don't use composer create-project, as it doesn't support installing a
@@ -19,7 +19,7 @@ class xhgui::download(
   composer::exec { 'xhgui':
     cmd       => 'install',
     cwd       => $dir,
-    user      => $user,
+    user      => $xhgui::params::www_user,
     scripts   => true,
     logoutput => on_failure, # So the more detailed Composer error message is shown
     require   => Vcsrepo[$dir],
